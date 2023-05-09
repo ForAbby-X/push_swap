e6f9e8877295fa93cd4af85b30c224e82e09207f
Alan DE FREITAS <alde-fre@e2r7p8.clusters.42paris.fr>
2023-05-09 18:21:11 +0200

    removed printf

--- src/checker/main.c
+++ src/checker/main.c
@@ -6,7 +6,7 @@
 /*   By: alde-fre <alde-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 12:54:08 by alde-fre          #+#    #+#             */
-/*   Updated: 2023/04/22 16:22:00 by alde-fre         ###   ########.fr       */
+/*   Updated: 2023/05/09 17:59:44 by alde-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int	main(int const argc, char const *const *const argv)
 		return (0);
 	context = context_create(argc - 1);
 	if (context == NULL)
-		return (printf("Error\n"), 1);
+		return (write(1, "Error\n", 6), 1);
 	if (parse_input(context->pile_a, argv + 1) == KO)
-		return (context_destroy(context), printf("Error\n"), 1);
+		return (context_destroy(context), write(1, "Error\n", 6), 1);
 	if (is_sorted(context->pile_a) == KO && execute_actions(context) == KO)
-		return (context_destroy(context), printf("Error\n"), 1);
+		return (context_destroy(context), write(1, "Error\n", 6), 1);
 	if (is_sorted(context->pile_a) == KO || context->pile_b->size != 0)
-		return (printf("KO\n"), 1);
+		return (write(1, "KO\n", 2), 1);
 	context_destroy(context);
-	return (printf("OK\n"), 0);
+	return (write(1, "KO\n", 2), 0);
 }
--- src/push_swap/main.c
+++ src/push_swap/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alde-fre <alde-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 20:09:42 by alde-fre          #+#    #+#             */
-/*   Updated: 2023/04/13 01:50:29 by mcourtoi         ###   ########.fr       */
+/*   Updated: 2023/05/09 17:58:50 by alde-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int	main(int const argc, char const *const *const argv)
 		return (0);
 	context = context_create(argc - 1);
 	if (context == NULL)
-		return (printf("Error\n"), 1);
+		return (write(1, "Error\n", 6), 1);
 	if (parse_input(context->pile_a, argv + 1) == KO)
-		return (context_destroy(context), printf("Error\n"), 1);
+		return (context_destroy(context), write(1, "Error\n", 6), 1);
 	if (solver(context) == KO)
-		return (context_destroy(context), printf("Error\n"), 1);
+		return (context_destroy(context), write(1, "Error\n", 6), 1);
 	context_destroy(context);
 	return (0);
 }
