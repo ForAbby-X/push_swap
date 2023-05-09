41dc0cee1337aa958e0b428bef5c6f11c63631b1
Alan DE FREITAS <alde-fre@e2r7p8.clusters.42paris.fr>
2023-05-09 19:59:43 +0200

    la fin

--- src/checker/main.c
+++ src/checker/main.c
@@ -6,7 +6,7 @@
 /*   By: alde-fre <alde-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 12:54:08 by alde-fre          #+#    #+#             */
-/*   Updated: 2023/05/09 17:59:44 by alde-fre         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:57:23 by alde-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int	main(int const argc, char const *const *const argv)
 		return (context_destroy(context), write(1, "Error\n", 6), 1);
 	if (is_sorted(context->pile_a) == KO && execute_actions(context) == KO)
 		return (context_destroy(context), write(1, "Error\n", 6), 1);
-	if (is_sorted(context->pile_a) == KO || context->pile_b->size != 0)
-		return (write(1, "KO\n", 2), 1);
+	if (context->pile_b->size != 0 || is_sorted(context->pile_a) == KO)
+		return (context_destroy(context), write(1, "KO\n", 3), 1);
 	context_destroy(context);
-	return (write(1, "KO\n", 2), 0);
+	return (write(1, "OK\n", 3), 0);
 }
